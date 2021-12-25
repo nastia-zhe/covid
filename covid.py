a1d78185c135332636e4eb6bc7f8374be3bb7ccb
Nastia <zherebtsova@gmail.com>
2021-12-25 21:04:32 +0300

    Скрипт доведен до ума

--- covid.py
+++ covid.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python
 
-from lib.initial_data import get_data
-from lib.tables import covid_cases_yesterday
+"""
+Пример вызова этого скрипта в терминале: python3 (или ./) covid.py -c China 'Russian Federation' Singapore -f some_table.txt
+"""
+
 import argparse
+from contextlib import redirect_stdout
+from lib.tables import (covid_cases_yesterday, get_data)
 
-#Получение стран (для которых нужно составить таблицу) из командной строки:
-parser = argparse.ArgumentParser() #создаем экземпляр парсера
+# Разбор аргументов командной строки
 
-parser.add_argument("--countries", '-c',nargs='+', default=['Japan']) #добавляем переменную 'countries', которой присваивается множество стран из ком.строки
+# создаем экземпляр парсера
+parser = argparse.ArgumentParser()
 
-args = parser.parse_args() #парсим аргументы из строки
+# добавляем переменную 'countries', которой присваивается одна/множество стран из ком.строки
+parser.add_argument("--countries", '-c', nargs='+', default=['Japan'])
 
-#------------------------------------------------------------------------
+# добавляем переменную 'filename', которой присваиватся имя файла для для записи в него результата выполнения функции (таблицы с данными по ковиду)
+parser.add_argument('--filename', '-f')
 
+# парсим аргументы из строки
+args = parser.parse_args()
+
+# Получить исходные данные для таблицы
 data = get_data()
-#------------------------------------------------------------------------
 
-#в командной строке нужно написать: python3 covid.py --countries China Japan Singapore
-#<python3> <назв.скрипта> <назв.переменной> <страны перечислить>
+# Привести данные к табличному виду
+result_table = covid_cases_yesterday(args.countries, data)
 
-covid_cases_yesterday(args.countries,data=data)
+# Печать итоговой таблицы в файл, если имя файла указано в командной строке (н-р: -f some_table.txt):
+if type(args.filename) == str:
+    with open(args.filename, 'w') as f:
+        with redirect_stdout(f):  # временное перенаправление стандартного вывода в файл
+            print(result_table)
+else:
+    print(result_table)
